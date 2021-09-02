690f47cbefb8c4a13546f13e63412c59aceae6f7
Yasser Dlewati <yasser@boostorder.com>
2021-09-02 20:09:13 +0800

    Added Get logic to Feature Controller

--- Controllers/FeatureController.cs
+++ Controllers/FeatureController.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using moneyLionAssignment.Models;
+using Newtonsoft.Json;
 
 namespace moneyLionAssignment.Controllers
 {
@@ -6,10 +12,36 @@ namespace moneyLionAssignment.Controllers
     [Route("[controller]")]
     public class FeatureController : ControllerBase
     {
+        private const string _dataFilePath = "./data.json";
+
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string featureName, string email)
         {
-            return Ok();
+            try
+            {
+                if (!string.IsNullOrEmpty(featureName) && !string.IsNullOrEmpty(email))
+                {
+                    var result = new object();
+                    if (System.IO.File.Exists(_dataFilePath))
+                    {
+                        using (StreamReader reader = new StreamReader(_dataFilePath))
+                        {
+                            var data = reader.ReadToEnd();
+                            var features = JsonConvert.DeserializeObject<List<Feature>>(data);
+                            result = features.Where(feature => feature.FeatureName == featureName && feature.Email == email)
+                                    .Select(f => f.IsEnabled);
+                        }
+                        
+                        return Ok(result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+
+            return NotFound();
         }
 
         [HttpPost]
